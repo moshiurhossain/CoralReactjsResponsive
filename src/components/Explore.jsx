92cf453a24eba15cbee72faf01fdceb541e85537
unknown <pantho883@gmail.com>
2025-07-26 01:46:31 +0600

    explore complete with responsive

---
+++ src/components/Explore.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import c10 from '../assets/images/c0.png'
+import c1 from '../assets/images/c1.png'
+import c2 from '../assets/images/c2.png'
+import c3 from '../assets/images/c3.png'
+import c4 from '../assets/images/c4.png'
+import { Link } from 'react-router'
+
+const Explore = () => {
+  return (
+    <section id='explore' className='lg:pb-[140px] '>
+        <div className="container">
+      <div className="explore-row flex  flex-col gap-6 lg:flex-row justify-center">
+    <div className='explore-left lg:w-[648px] w-[300px] lg:h-[648px] relative mx-auto lg:mx-0'>
+       <Link to={'#'} className='relative'><img src={c10} alt="exploreimage" /></Link> 
+        <h2 className='font-roboto font-normal lg:text-[34px] text-[15px] absolute bottom-[50%] left-[-30%] lg:left-[-40%] rotate-[-90deg]'>Explore new and popular styles</h2>
+    </div>
+    <div className='explore-right lg:w-[648px] w-[300px] mx-auto lg:mx-0 lg:h-[648px] flex flex-wrap  gap-6'>
+        <div className=" w-[290px] h-[280px]">
+                  <img  src={c1} alt="exploreimage" />
+             </div>
+        <div className=" w-[290px] h-[280px] ">
+                  <img  src={c2} alt="exploreimage" />
+             </div>
+        <div className=" w-[290px] h-[280px] ">
+                  <img  src={c3} alt="exploreimage" />
+             </div>
+        <div className=" w-[290px] h-[280px] ">
+                  <img  src={c4} alt="exploreimage" />
+             </div>
+    </div>
+      </div>
+        </div>
+        </section>
+  )
+}
+
+export default Explore
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Banner from '../components/Banner'
 import Brands from '../components/Brands'
+import Explore from '../components/Explore'
 
 const Home = () => {
   return (
     <div>
       <Banner/>
       <Brands/>
+      <Explore/>
     </div>
   )
 }

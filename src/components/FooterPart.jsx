65bbf4e527d605e580e8d8ffc1b6ddec6950a262
unknown <pantho883@gmail.com>
2025-07-31 01:21:03 +0600

    footer complete and responsive

--- src/components/FooterPart.jsx
+++ src/components/FooterPart.jsx
@@ -4,6 +4,7 @@ import { FaFacebookF } from "react-icons/fa6";
 import { FaTwitter } from "react-icons/fa";
 import { FaLinkedinIn } from "react-icons/fa";
 import { CiInstagram } from "react-icons/ci";
+import { Link } from 'react-router';
 
 
 const FooterPart = () => {
@@ -26,9 +27,37 @@ const FooterPart = () => {
             </div>
        </div>
        <div className='flex lg:flex-row flex-col lg:gap-[138px] gap-[38px]'>
-<div className="item2 mx-auto lg:mx-0 w-[200px] h-[212px] bg-amber-50"></div>
-       <div className="item2 mx-auto lg:mx-0 w-[200px] h-[212px] bg-amber-50"></div>
-       <div className="item2 mx-auto lg:mx-0 w-[200px] h-[212px] bg-amber-50"></div>
+<div className="item2 mx-auto lg:mx-0 w-[200px] h-[212px] ">
+  <h2 className=' font-opensans font-semibold text-base'>CATALOG</h2>
+  <div className="libox flex flex-col mt-[22px] gap-[12px]">
+    <Link to={'#'} className='font-opensans font-normal text-[12px] leading-[24px]'>Necklaces</Link>
+    <Link to={'#'} className='font-opensans font-normal text-[12px] leading-[24px]'>hoodies</Link>
+    <Link to={'#'} className='font-opensans font-normal text-[12px] leading-[24px]'>Jewelry Box</Link>
+    <Link to={'#'} className='font-opensans font-normal text-[12px] leading-[24px]'>t-shirt</Link>
+    <Link to={'#'} className='font-opensans font-normal text-[12px] leading-[24px]'>jacket</Link>
+  </div>
+</div>
+<div className="item2 mx-auto lg:mx-0 w-[200px] h-[212px] ">
+  <h2 className=' font-opensans font-semibold text-base'>ABOUT US</h2>
+  <div className="libox flex flex-col mt-[22px] gap-[12px]">
+    <Link to={'#'} className='font-opensans font-normal text-[12px] leading-[24px]'>Our Producers</Link>
+    <Link to={'#'} className='font-opensans font-normal text-[12px] leading-[24px]'>Sitemap</Link>
+    <Link to={'#'} className='font-opensans font-normal text-[12px] leading-[24px]'>FAQ</Link>
+    <Link to={'#'} className='font-opensans font-normal text-[12px] leading-[24px]'>About Us</Link>
+    <Link to={'#'} className='font-opensans font-normal text-[12px] leading-[24px]'>Terms & Conditions</Link>
+  </div>
+</div>
+<div className="item2 mx-auto lg:mx-0 w-[200px] h-[212px]">
+  <h2 className=' font-opensans font-semibold text-base'>CUSTOMER SERVICES</h2>
+  <div className="libox flex flex-col mt-[22px] gap-[12px]">
+    <Link to={'#'} className='font-opensans font-normal text-[12px] leading-[24px]'>Contact Us</Link>
+    <Link to={'#'} className='font-opensans font-normal text-[12px] leading-[24px]'>Sitemap</Link>
+    <Link to={'#'} className='font-opensans font-normal text-[12px] leading-[24px]'>Track Your Order</Link>
+    <Link to={'#'} className='font-opensans font-normal text-[12px] leading-[24px]'>Product Care & Repair</Link>
+    <Link to={'#'} className='font-opensans font-normal text-[12px] leading-[24px]'>Book an Appointmen</Link>
+  </div>
+</div>
+       
        </div>
        
     
